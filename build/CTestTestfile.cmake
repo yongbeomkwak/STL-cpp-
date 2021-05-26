70a75ff9c518f18f1c37c653dc66ebe4189d0c79
yongbeomkwak <yongbeomkwak@yongbeomkwakui-MacBookPro.local>
2021-05-26 19:13:21 +0900

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yongbeomkwak/Desktop/PLPlayGround/CPPSTL
+# Build directory: /Users/yongbeomkwak/Desktop/PLPlayGround/CPPSTL/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
